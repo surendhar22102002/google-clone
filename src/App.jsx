b844e6e622298309f65be9f9cdf6a1a91a9748a2
surendhar22102002 <loverboyffking143@gmail.com>
2024-03-14 09:41:49 -0700

    google-clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center text-red-600">
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default App
